e9a564d3993055a2e8f5822e1f2b8113f8b5f032
Phillip <pc494@cam.ac.uk>
2017-10-06 16:48:39 +0100

    cleaning directory structure

---
+++ tests/old_tests/test_diffraction_generator.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 The PyCrystEM developers
+#
+# This file is part of PyCrystEM.
+#
+# PyCrystEM is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyCrystEM is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyCrystEM.  If not, see <http://www.gnu.org/licenses/>.
+
+class TestDiffractionCalculator:
+
+    def setUp(self):
+        # Initialize electron diffraction calculator
+        pass
+
+    def test_calculate_ed_data(self):
+        # Calculate electron diffraction data
+        pass
+
+class TestDiffractionSimulation:
+
+    def setUp(self):
+        # Initialize a diffraction simulation object.
+
+    def test_calibration(self):
+        pass
+
+    def test_coordinates(self):
+        pass
+
+    def test_intensities(self):
+        pass
